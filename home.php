b7228243c352f41f8af74bf4bda319c33d7b1b7f
Niranjan <youthniranjan@gmail.com>
2013-10-20 11:13:24 +0545

    First upload

---
+++ home.php
@@ -0,0 +1,333 @@
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="shortcut icon" href="#">
+
+    <title>Home | VBS Nepal</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/navbar-fixed-top.css" rel="stylesheet">
+    <link href="css/custom.css" rel="stylesheet">
+    
+  </head>
+
+  <body>
+
+    <!-- Fixed navbar -->
+    <div class="navbar navbar-default navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">VBS Nepal</a>
+        </div>
+        <div class="navbar-collapse collapse">
+          <ul class="nav navbar-nav">
+            <li class="active"><a href="#">Home</a></li>
+            <li><a href="#about">About</a></li>
+            <li><a href="#contact">Contact</a></li>
+            <li class="dropdown">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
+              <ul class="dropdown-menu">
+                <li><a href="#">Action</a></li>
+                <li><a href="#">Another action</a></li>
+                <li><a href="#">Something else here</a></li>
+                <li class="divider"></li>
+                <li class="dropdown-header">Nav header</li>
+                <li><a href="#">Separated link</a></li>
+                <li><a href="#">One more separated link</a></li>
+              </ul>
+            </li>
+          </ul>
+          <ul class="nav navbar-nav navbar-right">
+            <li class="active"><a href="#">Social Media</a></li>
+          </ul>
+        </div><!--/.nav-collapse -->
+      </div>
+    </div>
+
+    <div class="container">
+      <!-- Main component for a primary marketing message or call to action -->
+        <div class="row">
+          <div class="col-md-8">
+            <h3>Virtual Book Store Nepal</h3>
+          </div>
+          <div class="col-md-4">
+            <form class="navbar-form" action="" method="POST">
+              <div class="form-group">
+              <input type="text" class="form-control search-query" placeholder="Search" autofocus required>
+              </div>
+              <div class="form-group">
+              <button class="btn btn-primary" type="submit">Search</button>
+              </div>
+            </form>
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="col-md-3">
+            <h4>Categories</h4><hr />
+            <h4><a href="#">Lorem Ipsum</a><br /></h4>
+            <h4><a href="#">Lorem Ipsum</a><br /></h4>
+            <h4><a href="#">Lorem Ipsum</a><br /></h4>
+            <h4><a href="#">Lorem Ipsum</a><br /></h4>
+            <h4><a href="#">Lorem Ipsum</a><br /></h4>
+            <h4><a href="#">Lorem Ipsum</a><br /></h4>
+            <h4><a href="#">Lorem Ipsum</a><br /></h4>
+            <h4><a href="#">Lorem Ipsum</a><br /></h4>
+            <h4><a href="#">Lorem Ipsum</a><br /></h4>
+            <h4><a href="#">Lorem Ipsum</a><br /></h4>
+            <h4><a href="#">Lorem Ipsum</a><br /></h4>
+            <h4><a href="#">Lorem Ipsum</a><br /></h4>
+            <h4><a href="#">Lorem Ipsum</a><br /></h4>
+            <h4><a href="#">Lorem Ipsum</a><br /></h4>
+            <h4><a href="#">Lorem Ipsum</a><br /></h4>
+            <h4><a href="#">Lorem Ipsum</a><br /></h4>
+            <h4><a href="#">Lorem Ipsum</a><br /></h4>
+            <h4><a href="#">Lorem Ipsum</a><br /></h4>
+            <h4><a href="#">Lorem Ipsum</a><br /></h4>
+            <h4><a href="#">Lorem Ipsum</a><br /></h4>
+            <h4><a href="#">Lorem Ipsum</a><br /></h4>
+          </div>
+          <div class="col-md-9">
+            <div class="tabbable">
+              <ul class="nav nav-tabs nav-justified">
+                <li class="active"><a href="#tabs1-pane1" data-toggle="tab">Latest</a></li>
+                <li><a href="#tabs1-pane2" data-toggle="tab">Featured</a></li>
+                <li><a href="#tabs1-pane3" data-toggle="tab">Best Seller</a></li>
+                <li><a href="#tabs1-pane4" data-toggle="tab">Trending</a></li>
+              </ul>
+              <div class="tab-content">
+                <div class="tab-pane active" id="tabs1-pane1">
+                  <div class="row">
+                    <div class="col-sm-6 col-md-3">
+                      <a href="#" class="thumbnail">
+                        <img alt="Generic placeholder thumbnail" src="img/Book.png">
+                      </a>
+                    </div>
+                    <div class="col-sm-6 col-md-3">
+                      <a href="#" class="thumbnail">
+                        <img alt="Generic placeholder thumbnail" src="img/Book.png">
+                      </a>
+                    </div>
+                    <div class="col-sm-6 col-md-3">
+                      <a href="#" class="thumbnail">
+                        <img alt="Generic placeholder thumbnail" src="img/Book.png">
+                      </a>
+                    </div>
+                    <div class="col-sm-6 col-md-3">
+                      <a href="#" class="thumbnail">
+                        <img alt="Generic placeholder thumbnail" src="img/Book.png">
+                      </a>
+                    </div>
+                  </div>
+                </div>
+                <div class="tab-pane" id="tabs1-pane2">
+                  <div class="row">
+                    <div class="col-sm-6 col-md-3">
+                      <a href="#" class="thumbnail">
+                        <img alt="Generic placeholder thumbnail" src="img/Book.png">
+                      </a>
+                    </div>
+                    <div class="col-sm-6 col-md-3">
+                      <a href="#" class="thumbnail">
+                        <img alt="Generic placeholder thumbnail" src="img/Book.png">
+                      </a>
+                    </div>
+                    <div class="col-sm-6 col-md-3">
+                      <a href="#" class="thumbnail">
+                        <img alt="Generic placeholder thumbnail" src="img/Book.png">
+                      </a>
+                    </div>
+                    <div class="col-sm-6 col-md-3">
+                      <a href="#" class="thumbnail">
+                        <img alt="Generic placeholder thumbnail" src="img/Book.png">
+                      </a>
+                    </div>
+                  </div>
+                </div>
+                <div class="tab-pane" id="tabs1-pane3">
+                  <div class="row">
+                    <div class="col-sm-6 col-md-3">
+                      <a href="#" class="thumbnail">
+                        <img alt="Generic placeholder thumbnail" src="img/Book.png">
+                      </a>
+                    </div>
+                    <div class="col-sm-6 col-md-3">
+                      <a href="#" class="thumbnail">
+                        <img alt="Generic placeholder thumbnail" src="img/Book.png">
+                      </a>
+                    </div>
+                    <div class="col-sm-6 col-md-3">
+                      <a href="#" class="thumbnail">
+                        <img alt="Generic placeholder thumbnail" src="img/Book.png">
+                      </a>
+                    </div>
+                    <div class="col-sm-6 col-md-3">
+                      <a href="#" class="thumbnail">
+                        <img alt="Generic placeholder thumbnail" src="img/Book.png">
+                      </a>
+                    </div>
+                  </div>
+                </div>
+                <div class="tab-pane" id="tabs1-pane4">
+                  <div class="row">
+                    <div class="col-sm-6 col-md-3">
+                      <a href="#" class="thumbnail">
+                        <img alt="Generic placeholder thumbnail" src="img/Book.png">
+                      </a>
+                    </div>
+                    <div class="col-sm-6 col-md-3">
+                      <a href="#" class="thumbnail">
+                        <img alt="Generic placeholder thumbnail" src="img/Book.png">
+                      </a>
+                    </div>
+                    <div class="col-sm-6 col-md-3">
+                      <a href="#" class="thumbnail">
+                        <img alt="Generic placeholder thumbnail" src="img/Book.png">
+                      </a>
+                    </div>
+                    <div class="col-sm-6 col-md-3">
+                      <a href="#" class="thumbnail">
+                        <img alt="Generic placeholder thumbnail" src="img/Book.png">
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="row">&nbsp;</div>
+            <div class="row">
+              <div class="col-md-5">
+                <img alt="Generic placeholder thumbnail" src="img/Book.png" width="100%" height="200">
+              </div>
+              <div class="col-md-7">
+                <div class="col-md-7">
+                  <h4>Popular Books</h4>
+                  <hr />
+                  <ul>
+                    <li>Lorem Ipsom</li>
+                    <li>Lorem Ipsom</li>
+                    <li>Lorem Ipsom</li>
+                    <li>Lorem Ipsom</li>
+                  </ul>
+                </div>
+                <div class="col-md-5">
+                  <img alt="Generic placeholder thumbnail" src="img/Book.png" width="100%" height="170">
+                </div>
+              </div>
+            </div>
+            <div class="row">
+              <br />
+              <h3>
+                "Lorem Ipum Dolor Sit amet, consectetur adipiscing elit. Sed at ante."
+              </h3>
+              <br />
+            </div>
+            <div class="row">
+              Sort By: 
+              <select>
+                <option value="Newest">Newest</option>
+                <option value="Trending">Trending</option>
+                <option value="Best Selling">Best Selling</option>
+                <option value="Trending">Trending</option>
+              </select> 
+              <hr />
+            </div>
+            <div class="row">
+              <div class="col-sm-6 col-md-3">
+                <a href="#" class="thumbnail">
+                  <img alt="Generic placeholder thumbnail" src="img/Book.png">
+                </a>
+                <p>Book Title</p>
+                <p>Auther Name</p>
+                <p>Rs. 500/-</p>
+              </div>
+              <div class="col-sm-6 col-md-3">
+                <a href="#" class="thumbnail">
+                  <img alt="Generic placeholder thumbnail" src="img/Book.png">
+                </a>
+                <p>Book Title</p>
+                <p>Auther Name</p>
+                <p>Rs. 500/-</p>
+              </div>
+              <div class="col-sm-6 col-md-3">
+                <a href="#" class="thumbnail">
+                  <img alt="Generic placeholder thumbnail" src="img/Book.png">
+                </a>
+                <p>Book Title</p>
+                <p>Auther Name</p>
+                <p>Rs. 500/-</p>
+              </div>
+              <div class="col-sm-6 col-md-3">
+                <a href="#" class="thumbnail">
+                  <img alt="Generic placeholder thumbnail" src="img/Book.png">
+                </a>
+                <p>Book Title</p>
+                <p>Auther Name</p>
+                <p>Rs. 500/-</p>
+              </div>
+            </div>
+            <div class="row">&nbsp;</div>
+                        <div class="row">
+              <div class="col-sm-6 col-md-3">
+                <a href="#" class="thumbnail">
+                  <img alt="Generic placeholder thumbnail" src="img/Book.png">
+                </a>
+                <p>Book Title</p>
+                <p>Auther Name</p>
+                <p>Rs. 500/-</p>
+              </div>
+              <div class="col-sm-6 col-md-3">
+                <a href="#" class="thumbnail">
+                  <img alt="Generic placeholder thumbnail" src="img/Book.png">
+                </a>
+                <p>Book Title</p>
+                <p>Auther Name</p>
+                <p>Rs. 500/-</p>
+              </div>
+              <div class="col-sm-6 col-md-3">
+                <a href="#" class="thumbnail">
+                  <img alt="Generic placeholder thumbnail" src="img/Book.png">
+                </a>
+                <p>Book Title</p>
+                <p>Auther Name</p>
+                <p>Rs. 500/-</p>
+              </div>
+              <div class="col-sm-6 col-md-3">
+                <a href="#" class="thumbnail">
+                  <img alt="Generic placeholder thumbnail" src="img/Book.png">
+                </a>
+                <p>Book Title</p>
+                <p>Auther Name</p>
+                <p>Rs. 500/-</p>
+              </div>
+            </div>
+          </div>
+        </div>
+    </div> <!-- /container -->
+    <footer>
+      <br />
+      <p class="pull-right"><a href="#">Back to top</a></p>
+      <p>&copy; Genese. &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></p>
+      <br />
+    </footer>
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="js/jquery.js"></script>
+    <script type="text/javaScript" src="js/bootstrap.min.js"></script>
+  </body>
+</html>
