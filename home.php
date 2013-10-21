ca1e78148929fb4386245f38b8b294f3ba7230fe
Niranjan <youthniranjan@gmail.com>
2013-10-21 12:50:24 +0545

    changes in structure
    I have made some changes in the file structures. The header, footer and
    body sections have been separated into different files. and also
    home.php has got some changes.

--- home.php
+++ home.php
@@ -1,62 +1,6 @@
-
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <link rel="shortcut icon" href="#">
-
-    <title>Home | VBS Nepal</title>
-
-    <!-- Bootstrap core CSS -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- Custom styles for this template -->
-    <link href="css/navbar-fixed-top.css" rel="stylesheet">
-    <link href="css/custom.css" rel="stylesheet">
-    
-  </head>
-
-  <body>
-
-    <!-- Fixed navbar -->
-    <div class="navbar navbar-default navbar-fixed-top">
-      <div class="container">
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-          <a class="navbar-brand" href="#">VBS Nepal</a>
-        </div>
-        <div class="navbar-collapse collapse">
-          <ul class="nav navbar-nav">
-            <li class="active"><a href="#">Home</a></li>
-            <li><a href="#about">About</a></li>
-            <li><a href="#contact">Contact</a></li>
-            <li class="dropdown">
-              <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
-              <ul class="dropdown-menu">
-                <li><a href="#">Action</a></li>
-                <li><a href="#">Another action</a></li>
-                <li><a href="#">Something else here</a></li>
-                <li class="divider"></li>
-                <li class="dropdown-header">Nav header</li>
-                <li><a href="#">Separated link</a></li>
-                <li><a href="#">One more separated link</a></li>
-              </ul>
-            </li>
-          </ul>
-          <ul class="nav navbar-nav navbar-right">
-            <li class="active"><a href="#">Social Media</a></li>
-          </ul>
-        </div><!--/.nav-collapse -->
-      </div>
-    </div>
-
+<?php 
+  include 'header.php';
+?>
     <div class="container">
       <!-- Main component for a primary marketing message or call to action -->
         <div class="row">
@@ -317,17 +261,6 @@
           </div>
         </div>
     </div> <!-- /container -->
-    <footer>
-      <br />
-      <p class="pull-right"><a href="#">Back to top</a></p>
-      <p>&copy; Genese. &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></p>
-      <br />
-    </footer>
-
-    <!-- Bootstrap core JavaScript
-    ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
-    <script src="js/jquery.js"></script>
-    <script type="text/javaScript" src="js/bootstrap.min.js"></script>
-  </body>
-</html>
+    <?php
+      include 'footer.php';
+    ?>
