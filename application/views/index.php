7e04ddf93a082ab775169eeaafdcb4ff7ca057fb
Mahesh Thapa <mahesh.gru03@gmail.com>
2013-10-27 21:47:52 +0545

    landing page by bibek

--- application/views/index.php
+++ application/views/index.php
@@ -1,40 +1,27 @@
+<html>
+<head>
+	<title>VBS | Welcome</title>
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/landing.css');?>">
+</head>
+<body>
+	<div id="logo-div">
+		<!-- <p id="logo"> <span> nepal</span> Reads</p> -->
+		<a href=""><img src="<?php echo base_url('assets/images/logo.png');?>"></a>
+	</div>
+	<div id="wrapper">
+		<div id="contents">
+			<!-- <p id="buttons"> <a href="">Login</a> <span><a href=""> Explore</a> </span></p> -->
+			<p id="head">Virtual Book Store</p>
+			<p id="quote">Buying Books Made Easier!! <br /></p>
+			<div id="butts">
+				<a href="#" id="login">Login</a>
+				<a href="home">Explore</a>
+			</div>
+			<!-- <input type="button" value="Login" class="button">
+			<input type="button" value="Explore" class="button"> -->
+		</div>
+	</div>
 
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <link rel="shortcut icon" href="#">
 
-    <title>Welcom | VBS Nepal</title>
-
-    <!-- Latest compiled and minified CSS -->
-    <link rel="stylesheet" href="<?php echo base_url('assets/css/bootstrap.min.css');?>">
-
-    <!-- Optional theme -->
-    <link rel="stylesheet" href="<?php echo base_url('assets/css/bootstrap-theme.min.css');?>">
-
-  </head>
-
-  <body>
-
-    <!-- Main jumbotron for a primary marketing message or call to action -->
-    <div class="jumbotron">
-      <div class="container">
-        <h1>VIRTUAL BOOK STORE</h1>
-        <p>Buying Books Made Easier</p><br />
-        <p>Reading is a good habit. For some, it has become a part of their precious life. Reading teaches a person to be wise, intellectual and smart. Some find meaning of life in books, and some try to make their life meaningful reading them.</p>
-        <p>It doesn't matter what you read for, but it is important that you read. And to make this reading more easy, we bring you VIRTUAL BOOK STORE.</p>
-        <a class="btn btn-primary btn-lg" href="login">Login &raquo;</a> <a class="btn btn-primary btn-lg" href="home">Explore &raquo;</a>
-      </div>
-    </div>
-
-    <!-- Bootstrap core JavaScript
-    ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
-    <!-- Latest compiled and minified JavaScript -->
-    <script src="<?php echo base_url('assets/js/bootstrap.min.js');?>"></script>
-  </body>
+</body>
 </html>
