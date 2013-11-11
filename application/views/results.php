092642f536cec1b466d5a8aa66d3a469b670e8d1
dothemuzic <dothemuzic@gmail.com>
2013-11-11 10:59:33 +0545

    improved search page.
    improved search page with seller and price details of a product.

--- application/views/results.php
+++ application/views/results.php
@@ -234,17 +234,63 @@
 					?>
                   </ul>
                 </div>
-                <div class="row-fluid product-info">
-                  <div class="module-container">
-                    <div class="close-button">x</div>
-                    <div class="pprev"><<<</div>
-                    <div class="nnext">>>></div>
+                <div class="row-fluid ">
+                  <div class="close-button">x</div>
+                  <div class="product-info">
+                      <div class="module-container">
+                    
+                    <!-- <div class="pprev"><</div>
+                    <div class="nnext">></div> -->
                     <div class="product-info-title">
-                      <h3 class="title">
-                        <a href="">This is a title for the book.And this may be long. this section is not complete hai mahesh!!</a>
-                      </h3>
+                      <p class="title">
+                        <a href="">Kara Walker: Pictures from another world</a>
+                      </p>
+                    </div>
+                    <div class="row-fluid product-info-row">
+                        <div class="span4 ">
+                          <div class="product-info-gallary">
+                            <div class="main-gallary">
+                            <img src="<?php echo $img;?>" alt="<?php echo $alt;?>">
+                          </div>
+                          </div>  
+                        </div>
+                        <div class="span8 ">
+                          <div class="product-info-details">
+                              <div class="row-fluid product-info-bla">
+                                <ul class="nav nav-tabs product-info-tabs" id="myTab">
+                                  <li ><a href="#information" data-toggle="tab">Information</a></li>
+                                  <li class="active"><a href="#price-comparison" data-toggle="tab">Price Comparision</a></li>
+                                </ul>
+                                <div class="tab-content product-info-detail">
+                                  <div class="tab-pane price-info"  id="information">
+                                      <p>this is a information tab</p>
+                                  </div>
+                                  <div class="tab-pane active price-info"  id="price-comparison">
+                                    <div class="row-fluid price-head">
+                                        <div class="span3 "> Seller</div>
+                                        <div class="span3 "> Price</div>
+                                        <div class="span3 "> Price with delivery</div>
+                                        <div class="span3 "></div>
+                                      </div><!-- end:price-head -->
+                                    <div class="row-fluid price-detail">
+                                        <div class="span3 product-seller"> Amazon</div>
+                                        <div class="span3 product-price"> $20.12</div>
+                                        <div class="span3 product-delivery"> $23.22</div>
+                                        <div class="span3 product-buy"><a href="">Buy from seller</a></div>
+                                    </div><!-- end:price-detail -->
+                                    <div class="row-fluid price-detail">
+                                        <div class="span3 product-seller"> Amazon</div>
+                                        <div class="span3 product-price"> $20.12</div>
+                                        <div class="span3 product-delivery"> Free Delivery</div>
+                                        <div class="span3 product-buy"><a href="">Buy from seller</a></div>
+                                    </div><!-- end:price-detail -->
+                                </div>
+                              </div> <!-- END: showcases -->
+                          </div>
+                        </div>
                     </div>
                   </div> <!-- end: module container -->
+                  </div> <!-- ends:product-info -->
                 </div>
                 <?php
 					}
@@ -294,6 +340,3 @@
     
 
 
-    
-          
-                    <hr class="footer-divider" />
