2f08720e5098e5318846b3963bf674ae55be10b6
Bidur <bidur_subedi@yahoo.com>
2013-11-11 21:20:04 +0545

    Cart Integration 4
    show the count of items in the cart in the nav bar

--- application/views/header.php
+++ application/views/header.php
@@ -39,7 +39,15 @@
                         <li><a href="">product</a></li>
                         <li><a href="">Dashboard</a></li>
                         <li><a href="">login</a></li>
-                        <li><a href="">Cart (0)</a></li>
+                        <?php 
+                          //To show the count of items in the cart
+                          if($this->session->userdata('cart')){
+                            $cart = $this->session->userdata('cart');
+                            $count= count($cart);
+                            $urlCart=base_url('view_cart');
+                            echo "<li><a href='$urlCart'>Cart ($count)</a></li>";
+                          }
+                        ?>
                         <li><a href="">Sign up</a></li>
                         <li><a href="">Sign in</a></li>
                       </ul>
--- application/views/product.php
+++ application/views/product.php
@@ -68,9 +68,6 @@
                       		<input type="submit" name="Cart" value="Add to Cart" id="add_to_cart" class="btn btn-small search-btn" />
                       	</form>
                       </p>
-                      <br/><pre>
-                        <?php print_r($this->session->all_userdata()); ?>
-                      </pre>
                       <br/>
 					  Price: <?php echo $shop_details[0][$i]['price'];?><br/>
                       Delivery cost within city: <?php echo $shop_details[0][$i]['delivery_cost_within_city'];?><br/>
